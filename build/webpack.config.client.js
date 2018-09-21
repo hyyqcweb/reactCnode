2bc8f52b85f2b9d890cbbbbf648bacab5e4e7598
hyyqc <junjie_hyyqc@163.com>
2018-09-21 10:59:27 +0800

    fix webpack.client devlopment

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin');
 
-module.exports = {
+// 判断是否为开发环境
+const isDev = process.env.NODE_ENV === 'development';
+
+const config = {
 	// 项目入口文件 主 app.js
 	entry: {
 		app: path.join(__dirname, '../client/app.js')
@@ -35,3 +38,21 @@ module.exports = {
 		})
 	]
 };
+
+if(isDev) {
+	config.devServer = {
+		host: '0.0.0.0', // 匹配 127.0.0.1, localhost, 本机IP
+		port: '8888',
+		contentBase: path.join(__dirname, '../dist'),
+		// hot: true,
+		overlay: {  // 出现错误,然后在网页中显示错误(黑色弹框)
+			errors: true
+		},
+		publicPath: '/public',
+		historyApiFallback: { // 所有404请求都返回 index.html
+			index: '/public/index.html'
+		}
+	}
+}
+
+module.exports = config;
